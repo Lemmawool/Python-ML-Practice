4d9139a6ee551e822e7c1cd7bb83023fbfc38877
Lemmawool <treyottaway@gmail.com>
2018-01-15 23:01:46 -0500

    Added featureNormalize

---
+++ exercise1/featureNormalize.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 15 20:03:25 2018
+
+@author: Trey
+"""
+
+import numpy as np
+
+def featureNormalize(X):
+  X_cols = len(X[0, ...])
+  mu = np.zeros(X_cols).T
+  X_norm = np.array(X)
+  sigma = np.array(mu)
+  
+  for i in range(X_cols):
+    mu[i] = np.mean(X[..., i])
+    sigma[i] = np.std(X[..., i], ddof=1)
+    X_norm[..., i] = (X[..., i] - mu[i]) / sigma[i]
+  
+  return X_norm
+
+
+
+  
---
+++ exercise1/pymlex2.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 15 20:02:17 2018
+
+@author: Trey
+"""
+
+import numpy as np
+import computeCost as cc
+import gradientDescent as gd
+import featureNormalize as fn
+
+data = np.loadtxt(open("data2.txt", "rb"), delimiter=",")
+
+num_features = len(data[1, ...])
+X = data[..., range(num_features - 1)]
+y = np.array([data[..., num_features - 1]]).T
+m = len(y)
+X_norm = fn.featureNormalize(X)
+X_norm = np.append(np.ones((m,1)), X_norm, axis=1)
+
+theta = np.zeros((3,1))
+theta = gd.gradientDescent(X_norm, y, theta, 0.01, 400)
+print(theta)
+
+
+
+
+
