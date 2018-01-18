d6d0a54a23eb3a688941cdc72a41d7ef5d79ac30
Lemmawool <treyottaway@gmail.com>
2018-01-18 18:33:18 -0500

    Started another exercise and cost function

---
+++ exercise2/ex2_functions.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 18 17:56:32 2018
+
+@author: Trey
+"""
+
+import math
+import numpy as np
+import scipy as sp
+
+def sigmoid(z):
+  return 1 / (1 + (math.e ** (-z)))
+
+def costFunc(X, y, theta):
+  m = len(y)
+  J = (1/m) * (-y.T @ sp.log(sigmoid(X @ theta)) - (1 - y).T @ sp.log(1 - sigmoid(X @ theta)))
+  return J
---
+++ exercise2/ex2_test.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 18 18:01:30 2018
+
+@author: Trey
+"""
+
+import numpy as np
+import ex2_functions as ex2
+
+data = np.loadtxt(open("data1.txt", "rb"), delimiter=",")
+num_features = len(data[1, ...])
+X = data[..., range(num_features - 1)]
+y = np.array([data[..., num_features - 1]]).T
+m = len(y)
+X = np.append(np.ones((m,1)), X, axis=1)
+
+theta = np.zeros((num_features,1))
+
+cost = ex2.costFunc(X, y, theta)
+print(cost)
+
+
+
+
